317c9e6a223959f82bd9f8b186039744538bd716
billyf <Maggie91>
2018-06-20 15:26:14 -0700

    Fixed id param issue and added link to owner edit page

--- routes/owners.js
+++ routes/owners.js
@@ -7,6 +7,7 @@ router
 	.route("/")
 	.get((req, res, next) => {
 		return Owner.find().then(owners => {
+			console.log(owners);
 			return res.render('index', {owners, resource: "Owner"});
 		});
 	})
@@ -23,24 +24,24 @@ router.route("/new").get((req, res, next) => {
 router
 	.route("/:ownerId")
 	.get((req, res, next) => {
-		return Owner.findById(req.params.id).then(owner => {
+		return Owner.findById(req.params.ownerId).then(owner => {
 			console.log(owner);
 			return res.render("show", {owner});
 		});
 	})
 	.patch((req, res, next) => {
-		return Owner.findByIdAndUpdate(req.params.id, req.body).then(() => {
+		return Owner.findByIdAndUpdate(req.params.ownerId, req.body).then(() => {
 			return res.redirect("/");
 		})
 	})
 	.delete((req, res, next) => {
-		return Owner.findByIdAndRemove(req.params.id).then(() => {
+		return Owner.findByIdAndRemove(req.params.ownerId).then(() => {
 			return res.redirect("/");
 		});
 	});
 
 router.route("/:ownerId/edit").get((req, res, next) => {
-	return Owner.findById(req.params.id).then(owner => {
+	return Owner.findById(req.params.ownerId).then(owner => {
 		return res.render("edit", {owner});
 	});
 });
